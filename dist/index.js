d20759fe5919fc9d814fef5de06b00354cd871fb
AlexanderZelaya <zelayajosue285@gmail.com>
2026-01-29 20:11:20 -0300

    POO

--- dist/index.js
+++ dist/index.js
@@ -1,70 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class POO {
-    constructor(_codigo = '', _nombre = '', _edad = 0, _laboratorio = 0, _parcial = 0) {
+    constructor(_codigo, _nombre, _edad, _laboratorio, _parcial) {
         this.codigo = _codigo;
         this.nombre = _nombre;
         this.edad = _edad;
         this.laboratorio = _laboratorio;
         this.parcial = _parcial;
     }
-    mostrarDatos() {
+    mostrarDATOS() {
         console.log("El estudiante: " + this.nombre);
-        console.log("Con codigo: " + this.codigo);
-        console.log("Tiene las siguientes notas de laboratorio: " + this.laboratorio + " parcial: " + this.parcial);
+        console.log("Codigo: " + this.codigo);
+        console.log("Tiene las siguientes notas - laboratorio: " + this.laboratorio, "parcial: " + this.parcial);
     }
-    mostrarNotas() {
-        console.log("lab: " + this.laboratorio + " parcial: " + this.parcial);
+    mostrarNOTAS() {
+        console.log("Tiene las siguientes notas - laboratorio: " + this.laboratorio, "parcial: " + this.parcial);
     }
 }
-//sacar el promedio de las notas y la suma es el resultado final de las notas
 class Redes {
-    constructor(_codigo = '', _nombre = '', _L1 = 0, _L2 = 0, _LA3 = 0, _P1 = 0, _P2 = 0, _P3 = 0) {
-        this.codigo = _codigo;
+    constructor(_nombre, _laboratorio1, _laboratorio2, _laboratorio3, _parcial1, _parcial2, _parcial3) {
         this.nombre = _nombre;
-        this.L1 = _L1;
-        this.L2 = _L2;
-        this.LA3 = _LA3;
-        this.P1 = _P1;
-        this.P2 = _P2;
-        this.P3 = _P3;
+        this.laboratorio1 = _laboratorio1;
+        this.laboratorio2 = _laboratorio2;
+        this.laboratorio3 = _laboratorio3;
+        this.parcial1 = _parcial1;
+        this.parcial2 = _parcial2;
+        this.parcial3 = _parcial3;
     }
-    sumaNotas() {
-        return this.L1 + this.L2 + this.LA3 + this.P1 + this.P2 + this.P3;
-    }
-    promedio() {
-        const cantidad = 6;
-        return this.sumaNotas() / cantidad;
-    }
-    mostrarDatos() {
-        console.log('El estudiante: ' + this.nombre);
-        console.log('Con codigo: ' + this.codigo);
-        console.log('Notas labs: L1:' + this.L1 + ' L2:' + this.L2 + ' LA3:' + this.LA3);
-        console.log('Parciales: P1:' + this.P1 + ' P2:' + this.P2 + ' P3:' + this.P3);
-        console.log('Suma de notas: ' + this.sumaNotas());
-        console.log('Promedio: ' + this.promedio());
-    }
-    mostrar() {
-        let prom1 = ((this.L1 * 0.60) + (this.P1 * 0.40));
-        let prom2 = ((this.L2 * 0.60) + (this.P2 * 0.40));
-        let prom3 = ((this.LA3 * 0.60) + (this.P3 * 0.40));
-        console.log('El promedio del primer parcial es: ' + prom1);
-        console.log('El promedio del segundo parcial es: ' + prom2);
-        console.log('El promedio del tercer parcial es: ' + prom3);
-        const sumaPromedios = prom1 + prom2 + prom3;
-        const promedioFinal = sumaPromedios / 3;
-        console.log('Suma de promedios: ' + sumaPromedios);
-        console.log('Promedio final (media de los 3 parciales): ' + promedioFinal);
+    MostrarDatos() {
+        const notaLaboratorio = ((this.laboratorio1 + this.laboratorio2 + this.laboratorio3) * 0.60) / 3;
+        const notaParcial = ((this.parcial1 + this.parcial2 + this.parcial3) * 0.40) / 3;
+        console.log(`${this.nombre} ${this.laboratorio1} ${this.laboratorio2} ${this.laboratorio3} ${this.parcial1} ${this.parcial2} ${this.parcial3} = ${notaLaboratorio + notaParcial}`);
     }
 }
-//objeto instancia a POO
-let estudiante1 = new POO('u20251084', 'JOSUE ALEXANDER ZELAYA RODRIGUEZ', 21, 9, 8);
-estudiante1.mostrarDatos();
-estudiante1.mostrarNotas();
-//objeto instancia a Redes 
-let estudiante2 = new Redes('u20251234', 'MARIA FERNANDA VALERIANO PEREZ', 10, 8.6, 10, 9.5, 10, 9);
-estudiante2.mostrarDatos();
-estudiante2.mostrar();
-console.log(estudiante1);
-console.log(estudiante2);
+const estudiante1 = new POO('U20251084', 'Josué Alexander(POCHO)', 21, 10, 9);
+estudiante1.mostrarDATOS();
+//estudiante1.mostrarNOTAS();
+const alumno1 = new Redes('Josué Alexander(POCHO)', 10, 10, 7, 8, 8, 10);
+alumno1.MostrarDatos();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,67 @@
 class POO {
-    codigo:string;
-    nombre:string;
+    codigo: string;
+    nombre: string;
     edad: number;
     laboratorio: number;
     parcial: number;
-    
-    constructor(_codigo:string = '', _nombre:string = '', _edad:number = 0,
-        _laboratorio:number = 0, _parcial:number = 0){
-            this.codigo=_codigo;
-            this.nombre=_nombre;
-            this.edad=_edad;
-            this.laboratorio=_laboratorio;
-            this.parcial=_parcial;
+
+    constructor(_codigo: string, _nombre: string, _edad: number, _laboratorio: number, _parcial: number) {
+        this.codigo = _codigo
+        this.nombre = _nombre
+        this.edad = _edad
+        this.laboratorio = _laboratorio
+        this.parcial = _parcial
     }
 
-    public mostrarDatos(): void {
+    public mostrarDATOS(): void {
         console.log("El estudiante: "+this.nombre);
-        console.log("Con codigo: "+this.codigo);
-        console.log("Tiene las siguientes notas de laboratorio: "+this.laboratorio+" parcial: "+this.parcial);
+        console.log("Codigo: "+this.codigo);
+        console.log("Tiene las siguientes notas - laboratorio: "+this.laboratorio, "parcial: "+this.parcial);
     }
 
-    public mostrarNotas(): void {
-        console.log("lab: "+this.laboratorio+" parcial: "+this.parcial)
+    public mostrarNOTAS(): void {
+        console.log("Tiene las siguientes notas - laboratorio: "+this.laboratorio, "parcial: "+this.parcial);
     }
 }
 
-//sacar el promedio de las notas y la suma es el resultado final de las notas
+
 class Redes {
-    codigo: string;
     nombre: string;
-    L1: number;
-    L2: number;
-    LA3: number;
-    P1: number;
-    P2: number;
-    P3: number;
+    laboratorio1: number;
+    laboratorio2: number;
+    laboratorio3: number;
+    parcial1: number;
+    parcial2: number;
+    parcial3: number;
 
-    constructor(_codigo: string = '', _nombre: string = '', _L1: number = 0, _L2: number = 0, _LA3: number = 0, _P1: number = 0, _P2: number = 0, _P3: number = 0) {
-        this.codigo = _codigo;
-        this.nombre = _nombre;
-        this.L1 = _L1;
-        this.L2 = _L2;
-        this.LA3 = _LA3;
-        this.P1 = _P1;
-        this.P2 = _P2;
-        this.P3 = _P3;
+    constructor(
+        _nombre: string,
+        _laboratorio1: number,
+        _laboratorio2: number,
+        _laboratorio3: number,
+        _parcial1: number,
+        _parcial2: number,
+        _parcial3: number
+    ) {
+        this.nombre = _nombre
+        this.laboratorio1 = _laboratorio1
+        this.laboratorio2 = _laboratorio2
+        this.laboratorio3 = _laboratorio3
+        this.parcial1 = _parcial1
+        this.parcial2 = _parcial2
+        this.parcial3 = _parcial3
     }
 
-    public sumaNotas(): number {
-        return this.L1 + this.L2 + this.LA3 + this.P1 + this.P2 + this.P3;
-    }
-
-    public promedio(): number {
-        const cantidad = 6; 
-        return this.sumaNotas() / cantidad;
-    }
-
-    public mostrarDatos(): void {
-        console.log('El estudiante: ' + this.nombre);
-        console.log('Con codigo: ' + this.codigo);
-        console.log('Notas labs: L1:' + this.L1 + ' L2:' + this.L2 + ' LA3:' + this.LA3);
-        console.log('Parciales: P1:' + this.P1 + ' P2:' + this.P2 + ' P3:' + this.P3);
-        console.log('Suma de notas: ' + this.sumaNotas());
-        console.log('Promedio: ' + this.promedio());
-    }
-
-    public mostrar(): void {
-        let prom1 = ((this.L1 * 0.60) + (this.P1 * 0.40));
-        let prom2 = ((this.L2 * 0.60) + (this.P2 * 0.40));
-        let prom3 = ((this.LA3 * 0.60) + (this.P3 * 0.40));
-        console.log('El promedio del primer parcial es: ' + prom1);
-        console.log('El promedio del segundo parcial es: ' + prom2);
-        console.log('El promedio del tercer parcial es: ' + prom3);
-        const sumaPromedios = prom1 + prom2 + prom3;
-        const promedioFinal = sumaPromedios / 3;
-        console.log('Suma de promedios: ' + sumaPromedios);
-        console.log('Promedio final (media de los 3 parciales): ' + promedioFinal);
+    public MostrarDatos(): void {
+        const notaLaboratorio = ((this.laboratorio1 + this.laboratorio2 + this.laboratorio3) * 0.60) / 3
+        const notaParcial = ((this.parcial1 + this.parcial2 + this.parcial3) * 0.40) / 3
+        console.log(`${this.nombre} ${this.laboratorio1} ${this.laboratorio2} ${this.laboratorio3} ${this.parcial1} ${this.parcial2} ${this.parcial3} = ${notaLaboratorio + notaParcial}`);
     }
 }
 
-//objeto instancia a POO
-let estudiante1 = new POO('u20251084', 'JOSUE ALEXANDER ZELAYA RODRIGUEZ', 21, 9, 8);
-estudiante1.mostrarDatos();
-estudiante1.mostrarNotas();
-//objeto instancia a Redes 
-let estudiante2 = new Redes('u20251234', 'MARIA FERNANDA VALERIANO PEREZ', 10, 8.6, 10, 9.5, 10, 9);
-estudiante2.mostrarDatos();
-estudiante2.mostrar();
+const estudiante1 = new POO('U20251084','Josué Alexander(POCHO)',21,10,9);
+estudiante1.mostrarDATOS();
+//estudiante1.mostrarNOTAS();
+const alumno1 = new Redes('Josué Alexander(POCHO)', 10,10,7,8,8,10);
+alumno1.MostrarDatos();
 
-console.log(estudiante1);
-console.log(estudiante2);
